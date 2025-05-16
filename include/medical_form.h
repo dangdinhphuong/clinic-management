0763fba9cbd7678694a8929a0fed19faae80b9fa
Phongvt040305 <vp040305@gmail.com>
2025-05-16 07:54:06 +0700

    Update medical_form.h

---
+++ include/medical_form.h
@@ -0,0 +1,8 @@
+// ------------------- Cấu trúc phiếu khám -------------------
+struct MedicalForm {
+    string ;
+    string patientId;
+    string date;
+    string diagnosis;
+    MedicalForm* next;
+};
