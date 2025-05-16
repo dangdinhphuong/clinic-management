6f6329a5b296340fdeebecea5f385e6959c89c42
Phongvt040305 <vp040305@gmail.com>
2025-05-16 08:13:26 +0700

    Update file_handler.cpp

--- src/file_handler.cpp
+++ src/file_handler.cpp
@@ -1,3 +1,39 @@
+// Ghi danh sách phiếu khám ra file
+void saveFormsToFile(MedicalForm* head, const string& filename) {
+    ofstream file(filename);
+    while (head) {
+        file << head->formId << "," << head->patientId << ","
+             << head->date << "," << head->diagnosis << "\n";
+        head = head->next;
+    }
+    file.close();
+    cout << "Da ghi danh sach phieu kham vao file.\n";
+}
+
+// Đọc danh sách phiếu khám từ file
+void loadFormsFromFile(MedicalForm*& head, const string& filename) {
+    ifstream file(filename);
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        MedicalForm* newNode = new MedicalForm;
+        getline(ss, newNode->formId, ',');
+        getline(ss, newNode->patientId, ',');
+        getline(ss, newNode->date, ',');
+        getline(ss, newNode->diagnosis);
+        newNode->next = nullptr;
+
+        if (!head) head = newNode;
+        else {
+            MedicalForm* temp = head;
+            while (temp->next) temp = temp->next;
+            temp->next = newNode;
+        }
+    }
+    file.close();
+    cout << "Da doc danh sach phieu kham tu file.\n";
+}
+
 // Ghi danh sách bệnh nhân ra file
 void savePatientsToFile(Patient* head, const string& filename) {
     ofstream file(filename);
@@ -10,41 +46,6 @@ void savePatientsToFile(Patient* head, const string& filename) {
 }
 
 // Đọc danh sách bệnh nhân từ file
-void loadPatientsFromFile(Patient*& head, const string& filename) {
-    ifstream file(filename);
-    string line;
-    while (getline(file, line)) {
-        stringstream ss(line);
-        Patient* newNode = new Patient;
-        string ageStr;
-        getline(ss, newNode->id, ',');
-        getline(ss, newNode->name, ',');
-        getline(ss, ageStr, ',');
-        newNode->age = stoi(ageStr);
-        getline(ss, newNode->gender);
-        newNode->next = nullptr;
-
-        if (!head) head = newNode;
-        else {
-            Patient* temp = head;
-            while (temp->next) temp = temp->next;
-            temp->next = newNode;
-        }
-    }
-    file.close();
-    cout << "Da doc danh sach benh nhan tu file.\n";
-}
-
-void savePatientsToFile(Patient* head, const string& filename) {
-    ofstream file(filename);
-    while (head) {
-        file << head->id << "," << head->name << "," << head->age << "," << head->gender << "\n";
-        head = head->next;
-    }
-    file.close();
-    cout << "Da ghi danh sach benh nhan vao file.\n";
-}
-
 void loadPatientsFromFile(Patient*& head, const string& filename) {
     ifstream file(filename);
     string line;
